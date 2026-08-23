73c4f12c667713381d55726420490d54a96c1a07
invitacionesweb <invitacionesweb.info@gmail.com>
2026-08-23 19:50:48 -0300

    A
    A

---
+++ config.js
@@ -0,0 +1,42 @@
+// =============================================
+//  CONFIGURACIÓN DE LA INVITACIÓN - ROMA
+//  Editá solo este archivo para cada cliente
+// =============================================
+
+const CONFIG = {
+
+  // --- DATOS DEL EVENTO ---
+  nombreFestejado: "Amapola",
+  subtitulo: "Te esperamos",
+
+  // --- FECHA PARA EL CONTADOR (ISO 8601) ---
+  fechaContador: "2026-09-06T23:59:59",
+
+  // --- UBICACIÓN ---
+textoUbicacion: "Falta poquito para mi llegada, mis papis están ansiosos y con tu cariño y compañia este momento será aún más especial. <br> Te esperamos de 13:00 hs a 17:00 hs en Las Bermudas 154 Guernica.",
+textoUbicacion2: "",
+
+  linkMapa: "https://www.google.com/maps/dir//Las+Bermudas+154,+B1862DKD+Guernica,+Provincia+de+Buenos+Aires/@-34.9341366,-58.3773523,17z/data=!4m17!1m7!3m6!1s0x95bd2b2a7c25b6af:0x6c3f51663d6afaaf!2sLas+Bermudas+154,+B1862DKD+Guernica,+Provincia+de+Buenos+Aires!3b1!8m2!3d-34.9341366!4d-58.3747774!4m8!1m0!1m5!1m1!1s0x95bd2b2a7c25b6af:0x6c3f51663d6afaaf!2m2!1d-58.3747774!2d-34.9341366!3e0?entry=ttu&g_ep=EgoyMDI2MDgxOS4wIKXMDSoASAFQAw%3D%3D",
+  linkAgendar: "",
+
+  // --- FORMULARIO ---
+  textoForm: "Tu presencia es lo más importante",
+  textoFormSub: "No olvides confirmar asistencia con mis papis para una mejor organización",
+  maxInvitados: 5,
+  scriptFormUrl: "https://script.google.com/macros/s/AKfycbyqKEbEHYGqSm9W1vtyhU-tzQbKZG_nMFEyPkZniFNgMYqt7lNsg4I6pfxTNHApEPgY/exec",
+
+  // --- AUDIO ---
+  archivoAudio: "./AMAPOLA.mp3",
+
+  // --- COLORES ---
+  colores: {
+    color0: "#ffffff",
+    color1: "#E4C7C5",
+    color2: "#f8f3ed",
+    color3: "#E4C7C5",
+  },
+
+  // --- TÍTULO DE LA PESTAÑA ---
+  tituloTab: "Amapola",
+  
+};
