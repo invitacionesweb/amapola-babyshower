a91974c2068d1cf96d3ec8d153d77e2d8f307fca
invitacionesweb <invitacionesweb.info@gmail.com>
2026-08-23 20:14:35 -0300

    a
    a

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
   fechaContador: "2026-09-06T23:59:59",
 
   // --- UBICACIÓN ---
-textoUbicacion: "Falta poquito para mi llegada, mis papis están ansiosos y con tu cariño y compañia este momento será aún más especial. <br> Te esperamos de 13:00 hs a 17:00 hs en Las Bermudas 154 Guernica.",
+textoUbicacion: "Falta poquito para mi llegada, mis papis están muy ansiosos y agradecen todo el amor que nos brindaste en mi dulce espera. <br> Por eso espero que nos acompañes para hacer este momento aún más especial. <br> Te esperamos de 13:00 hs a 17:00 hs en Las Bermudas 154 Guernica.",
 textoUbicacion2: "",
 
   linkMapa: "https://www.google.com/maps/dir//Las+Bermudas+154,+B1862DKD+Guernica,+Provincia+de+Buenos+Aires/@-34.9341366,-58.3773523,17z/data=!4m17!1m7!3m6!1s0x95bd2b2a7c25b6af:0x6c3f51663d6afaaf!2sLas+Bermudas+154,+B1862DKD+Guernica,+Provincia+de+Buenos+Aires!3b1!8m2!3d-34.9341366!4d-58.3747774!4m8!1m0!1m5!1m1!1s0x95bd2b2a7c25b6af:0x6c3f51663d6afaaf!2m2!1d-58.3747774!2d-34.9341366!3e0?entry=ttu&g_ep=EgoyMDI2MDgxOS4wIKXMDSoASAFQAw%3D%3D",
